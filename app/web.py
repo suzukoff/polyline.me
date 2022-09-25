dd8a9741f934c73ef07a93af8aabf34291e6fbed
= <=>
2022-09-25 12:52:54 +0000

    main.pyハンドラ概要まで完成。

--- app/web.py
+++ app/web.py
@@ -1,23 +1,26 @@
-from fastapi import FastAPI, Header, Response, status
 import os
+import re
+from fastapi import FastAPI, Header, Response, status
+from fastapi.responses import FileResponse
+
+import app.global_var as g
 
 def handler(webPath, response):
-	WWWROOT = os.environ.get("WWWROOT")
 
-	extension = os.path.splitext(webPath)[1][1:]
 
-	extDict = {
-		"html": "text/html",
-		"css": "text/css",
-		"py": "text/py-script"
-	}
-
-	if (extension not in extDict):
+	# 【ディレクトリトラバーサル攻撃対策】-> ドット連続を禁止
+	if re.match(r'\.{2,}', webPath):
 		response.status_code = status.HTTP_404_NOT_FOUND
 		return ""
 
-	response.headers["Content-Type"] = ""
+	filepath = g.WWWROOT + webPath
 
-	return WWWROOT
+	# 指定したファイルが存在しなければ、、、
+	if not os.path.isfile(filepath):
+		response.status_code = status.HTTP_404_NOT_FOUND
+		return ""
+
+	return FileResponse(filepath)
+
 
 
--- main.py
+++ main.py
@@ -1,26 +1,51 @@
 from typing import Union
 from fastapi import FastAPI, Header, Response, status
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
-
-import os
-from dotenv import load_dotenv
+from enum import Enum
 
 import app.index as index
 import app.web as web
 
+# 環境変数を共有
+import app.global_var as g
 
-load_dotenv()
-
+# アプリケーションオブジェクトインスタンスの生成
 app = FastAPI()
 
+
+# インデックスページ
 @app.get("/")
 def indexHandler():
-    return index.handler()
+    return FileResponse(g.WWWROOT + "html/index/index.html")
 
 
+
+# 静的ファイル
 @app.get("/web/{webPath:path}")
 def webHandler(webPath: str, response: Response):
     return web.handler(webPath, response)
 
 
 
+# メンバー限定ページ
+class memberOnly(str, Enum):
+    profile = "profile",
+    chat = "chat",
+    conversation = "conversation"
+    history = "history",
+
+
+@app.get("/{directive}")
+def hello_world(directive:memberOnly):
+    if directive == memberOnly.profile:
+        result = profile.handler()
+    elif directive == memberOnly.chat:
+        result = chat.handler()
+    elif directive == memberOnly.conversation:
+        result = conversation.handler()
+    elif directive == memberOnly.history:
+        result = history.handler()
+    else:
+        # 例外
+    return result
